f16005844882baffcd46cb63538aeaf097c72994
Nickkick909 <nicholasknight1999@yahoo.com>
2021-10-19 11:51:14 -0400

    Updating the fetchApiKey function to just use a known api key

--- src/api/scrapers/fetchapikey.js
+++ src/api/scrapers/fetchapikey.js
@@ -3,39 +3,39 @@ const { checkRes, parseJSON } = require('../../utils');
 const request = util.promisify(require('postman-request'));
 
 module.exports = async (options = {}) => {
-    const { limit, proxy, userAgent, cookieJar } = options;
+    // const { limit, proxy, userAgent, cookieJar } = options;
 
-    const reqOptions = {
-        url: 'https://www.stockx.com/',
-        headers: {
-            'Host': 'stockx.com',
-            'upgrade-insecure-requests': '1',
-            'user-agent': userAgent,
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'sec-fetch-site': 'none',
-            'accept-language': 'en-US,en;q=0.9'
-        },
-        //jar: cookieJar,
-        followAllRedirects: true,
-        followRedirect: true,
-        proxy
-    };
+    // const reqOptions = {
+    //     url: 'https://www.stockx.com/',
+    //     headers: {
+    //         'Host': 'stockx.com',
+    //         'upgrade-insecure-requests': '1',
+    //         'user-agent': userAgent,
+    //         'sec-fetch-mode': 'navigate',
+    //         'sec-fetch-user': '?1',
+    //         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    //         'sec-fetch-site': 'none',
+    //         'accept-language': 'en-US,en;q=0.9'
+    //     },
+    //     //jar: cookieJar,
+    //     followAllRedirects: true,
+    //     followRedirect: true,
+    //     proxy
+    // };
 
-    const res = await request(reqOptions);
+    // const res = await request(reqOptions);
 
-    checkRes(res);
+    // checkRes(res);
 
-    let globalConstantString, constantsObject;
+    // let globalConstantString, constantsObject;
     
-    try {
-        globalConstantString = res.body.split('window.globalConstants = ')[1].split('</script>')[0].trim();
-        constantsObject = globalConstantString.endsWith(';') ? parseJSON(globalConstantString.slice(0, globalConstantString.length - 1)) : JSON.parse(globalConstantString);
-    }
-    catch(err){
-        throw new Error("Failed to parse API key for search");
-    };
+    // try {
+    //     globalConstantString = res.body.split('window.globalConstants = ')[1].split('</script>')[0].trim();
+    //     constantsObject = globalConstantString.endsWith(';') ? parseJSON(globalConstantString.slice(0, globalConstantString.length - 1)) : JSON.parse(globalConstantString);
+    // }
+    // catch(err){
+    //     throw new Error("Failed to parse API key for search");
+    // };
 
-    return constantsObject.search.SEARCH_ONLY_API_KEY;
+    return "Njg4YjI1NTNhMTAzMzRlZjc4N2UyZmVjNzU4ODY3ZWY1OGU5YmEzZDBkNGFkMzE0NWQzMmEwZjllOWNlOTZkY3ZhbGlkVW50aWw9MTYzNDQ4Mzk4OA==";
 };
